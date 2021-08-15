07c4a05d7b7ab0ea0b7f46e80e57062e2d025d06
Alisson V. de Souza <alissonvsouza8@gmail.com>
2021-08-15 19:06:45 -0300

    Add prettier style code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
-import { Application } from 'express'
+import { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
 
-
 export class SetupServer extends Server {
-
   constructor(private port = 3000) {
     super();
   }
