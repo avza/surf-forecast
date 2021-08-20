630594b754d1ed806ecccd6e096eda660252af54
Alisson V. de Souza <alissonvsouza8@gmail.com>
2021-08-20 05:53:37 -0300

    Add beach model

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
-import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
+import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -22,7 +23,8 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachController = new BeachesController();
+    this.addControllers([forecastController, beachController]);
   }
 
   private async databaseSetup(): Promise<void> {
