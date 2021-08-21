0016aa577fe41492d6dbb9be24ceb74a4cb7d769
Alisson V. de Souza <alissonvsouza8@gmail.com>
2021-08-21 16:40:00 -0300

    Add authenticate of users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { BaseController } from './index';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,27 @@ export class UsersController extends BaseController {
     } catch (err) {
       this.sendCreateUpdateErrorResponse(res, err);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send({ code: 401, error: 'User not found!' });
+    }
+
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      return res
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' });
+    }
+
+    const token = await AuthService.generateToken(user.toJSON());
+    return res.status(200).send({ token });
   }
 }
